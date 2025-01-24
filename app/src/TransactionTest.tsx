f33715f50efb3bbcb8e422beea5749e1ae739124
Louis Moulin <louis.moulin@outlook.fr>
2025-01-24 01:58:36 +0100

    handle assign instruction

--- app/src/TransactionTest.tsx
+++ app/src/TransactionTest.tsx
@@ -6,6 +6,7 @@ import { BN } from 'bn.js';
 
 export default function TransactionTest() {
   const [count, setCount] = useState(new BN(0));
+  const [assignValue, setAssignValue] = useState('0'); // Declare a state variable...
   const program = useProgram();
   const wallet = useWallet();
   const onClickInit = async () => {
@@ -36,6 +37,14 @@ export default function TransactionTest() {
     console.log('tx increment: ', tx)
     await onClickFetch();
   }
+  const onClickAssign = async () => {
+    const tx = await program?.methods.assign(new BN(assignValue)).accounts({
+      counter: counterAccount.publicKey
+    }).rpc();
+    console.log('tx increment: ', tx)
+    await onClickFetch();
+
+  }
 
   return (
     <div>
@@ -44,11 +53,12 @@ export default function TransactionTest() {
         <button onClick={onClickInit}>
           Init
         </button>
-        <button onClick={onClickFetch}>
-          Fetch Data
-        </button>
         <button onClick={onClickIncrement}>
           Increment
+        </button>
+        <button onClick={onClickAssign}>
+          <input name="assign-input" value={assignValue} onChange={(e) => setAssignValue(e.target.value)} />
+          Assign a value
         </button>
         count = {count.toString()}
       </div>
